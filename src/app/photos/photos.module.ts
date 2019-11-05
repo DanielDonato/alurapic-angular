6c74a26cd13e8e31139537ff010360c7aaf57784
DanielDonato <daniel_donato10@yahoo.com.br>
2019-11-05 20:57:51 -0300

    Refarando imports em module

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from "@angular/core";
 import { PhotoComponent } from "./photo/photo.component";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
     declarations: [PhotoComponent],
-    exports: [ PhotoComponent ] //torna o component acessível a quem importar o module
+    exports: [ PhotoComponent ], //torna o component acessível a quem importar o module
+    imports: [ HttpClientModule ]
 })
 export class PhotosModule {}
