558435fdb36417c6ebec9d147915bb6e4a0099df
DanielDonato <daniel_donato10@yahoo.com.br>
2019-11-05 20:56:32 -0300

    Tipando retorno da api, e usando ciclo de vida

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Injectable } from "@angular/core";
+import { Photo } from "./photo";
 
 @Injectable({providedIn: 'root'})
 export class PhotoService {
@@ -8,8 +9,8 @@ export class PhotoService {
         private http: HttpClient
     ) {}
 
-    listFromUser(username: string): Observable<Object[]> {
-        return this.http.get<Object[]>(`http://localhost:3000/${username}/photos`);
+    listFromUser(username: string): Observable<Photo[]> {
+        return this.http.get<Photo[]>(`http://localhost:3000/${username}/photos`);
     }
 
 }
