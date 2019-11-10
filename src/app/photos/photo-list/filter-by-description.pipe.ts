66432fc44faab23764f0697d0253d96f0736738d
DanielDonato <daniel_donato10@yahoo.com.br>
2019-11-10 17:58:28 -0300

    Criando pipe

---
+++ src/app/photos/photo-list/filter-by-description.pipe.ts
@@ -0,0 +1,24 @@
+import { Pipe, PipeTransform } from "@angular/core";
+import { Photo } from "../photo/photo";
+
+@Pipe({
+    name: 'filterByDescription'
+})
+export class FilterByDescription implements PipeTransform {
+
+    transform(photos: Photo[], descriptionQuery: string) {
+        // primeiro parametro: variavel que será aplicada o pipe
+        // segundo parametro: valor de parametros recebido pelo pipe
+        descriptionQuery = descriptionQuery
+            .trim()
+            .toLowerCase();
+
+        if (!descriptionQuery) {
+            return photos;
+        }
+        return photos.filter(photo =>
+            photo.description.toLowerCase().includes(descriptionQuery)
+        );
+    }
+
+}
