e6d37226f4ad6d0a08135b48e1a233e7347114a2
DanielDonato <daniel_donato10@yahoo.com.br>
2019-11-15 02:13:48 -0300

    Componentizando mensagens de validação

---
+++ src/app/shared/components/vmessage/vmessage.module.ts
@@ -0,0 +1,13 @@
+import { NgModel } from "@angular/forms";
+import { NgModule } from "@angular/core";
+import { VMessageComponent } from "./vmessage.component";
+
+@NgModule({
+    declarations: [
+        VMessageComponent
+    ],
+    exports: [
+        VMessageComponent
+    ]
+})
+export class VMessageModule { }
