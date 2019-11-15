33226a1ff90dc4744dfe601227b5d351682960a8
DanielDonato <daniel_donato10@yahoo.com.br>
2019-11-15 19:54:16 -0300

    Acessando e armazenando token

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import { tap } from 'rxjs/operators';
+import { TokenService } from '../token/token.service';
 
 const API_URL = "http://localhost:3000"
 @Injectable({
@@ -9,10 +10,18 @@ const API_URL = "http://localhost:3000"
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+    private tokenService: TokenService) { }
 
   authenticate(username: string, password: string): Observable<any> {
-    return this.http.post(API_URL + '/user/login', {userName: username, password});
+    return this.http.post(API_URL + '/user/login',
+        {userName: username, password} ,
+        { observe: 'response' }
+      )
+      .pipe(tap(res => {
+        const authToken = res.headers.get('x-access-token');
+        this.tokenService.setToken(authToken);
+      }));
   }
 
 }
