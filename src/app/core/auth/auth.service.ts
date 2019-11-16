72b906372c503f55629fbee63d5298be68e2f5de
DanielDonato <daniel_donato10@yahoo.com.br>
2019-11-16 01:27:21 -0300

    Exibindo nome do usuario logado atraves do BehaviorSubject

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
-import { TokenService } from '../token/token.service';
+import { UserService } from '../user/user.service';
 
 const API_URL = "http://localhost:3000"
 @Injectable({
@@ -11,7 +11,7 @@ const API_URL = "http://localhost:3000"
 export class AuthService {
 
   constructor(private http: HttpClient,
-    private tokenService: TokenService) { }
+    private userService: UserService) { }
 
   authenticate(username: string, password: string): Observable<any> {
     return this.http.post(API_URL + '/user/login',
@@ -20,7 +20,7 @@ export class AuthService {
       )
       .pipe(tap(res => {
         const authToken = res.headers.get('x-access-token');
-        this.tokenService.setToken(authToken);
+        this.userService.setToken(authToken);
       }));
   }
 
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,9 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "../user/user.service";
+import { Observable } from "rxjs";
+import { User } from "../user/user";
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+    user$: Observable<User>;
+    user: User;
+    
+    constructor(private userService: UserService) { }
+
+    ngOnInit(): void {
+        this.user$ = this.userService.getUser();
+        this.user$.subscribe(user => this.user = user);
+    }
 
 }
