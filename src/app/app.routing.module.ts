603f0a725b547a9c396dcd140c26b84cfd7c08a7
DanielDonato <daniel_donato10@yahoo.com.br>
2019-11-17 15:06:16 -0300

    Component singup

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -7,9 +7,11 @@ import { NotFoundComponent } from "./errors/not-found/not-found.component";
 import { PhotoListResolver } from "./photos/photo-list/photo-list.resolver";
 import { SignInComponent } from "./home/signin/signin.component";
 import { AuthGuard } from "./core/auth/auth.guard";
+import { SingupComponent } from "./home/singup/singup.component";
 
 const routes: Routes = [
     {path: '', component: SignInComponent, canActivate: [AuthGuard]},
+    {path: 'singup', component: SingupComponent, canActivate: [AuthGuard]},
     {path: 'user/:username', component: PhotoListComponent, resolve: {photos: PhotoListResolver}},
     {path: 'p/add', component: PhotoFormComponent},
     {path: '**', component: NotFoundComponent} // ** qualquer outra rota que nao seja as listadas
