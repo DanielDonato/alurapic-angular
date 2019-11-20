ac71523c089afe4dea4f7b722a208602a6b6e2a7
DanielDonato <daniel_donato10@yahoo.com.br>
2019-11-19 23:35:55 -0300

    Adquando exibicao de imagens

--- src/app/photos/photo/photo.component.ts
+++ src/app/photos/photo/photo.component.ts
@@ -1,14 +1,28 @@
 import { Component, Input } from '@angular/core';
 
+const CLOUD = 'http://localhost:3000/imgs/';
+
 @Component({
     selector: 'app-photo',
     templateUrl: 'photo.component.html'
 })
 export class PhotoComponent {
 
+
+  private _url = '';
   @Input()
   description = '';
 
   @Input()
-  url = '';
+  set url(url: string) { // inbound properties em no set
+    if (!url.startsWith('data')){
+        this._url = CLOUD + url; // regra da api atual
+    } else {
+      this._url = url;
+    }
+  }
+
+  get url() {
+    return this._url;
+  }
 }
