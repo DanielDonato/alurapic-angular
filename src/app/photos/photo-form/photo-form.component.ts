b812cf6fcae347d857d7835baceeee7a94b8a1e0
DanielDonato <daniel_donato10@yahoo.com.br>
2019-11-19 23:30:19 -0300

    Upload de foto

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PhotoService } from '../photo/photo.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-photo-form',
@@ -10,8 +12,11 @@ export class PhotoFormComponent implements OnInit {
 
 
   photoForm: FormGroup;
+  file: File;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder,
+    private photoService: PhotoService,
+    private router: Router) { }
 
   ngOnInit() {
     this.photoForm = this.formBuilder.group({
@@ -19,6 +24,14 @@ export class PhotoFormComponent implements OnInit {
       description: ['', Validators.maxLength(300)],
       allowComments: [true]
     });
+  }
+
+  upload() {
+    const description = this.photoForm.get('description').value;
+    const allowComments = this.photoForm.get('allowComments').value;
+    this.photoService
+      .upload(description, allowComments, this.file)
+      .subscribe(() => this.router.navigate(['']));
   }
 
 }
